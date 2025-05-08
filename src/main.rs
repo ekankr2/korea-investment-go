a7c73316b2be981237f373de8011ea15003bccef
IkHwan Im <ekankr2@naver.com>
2025-05-08 21:33:28 +0900

    Add dotenv and environment variable support
    Integrated dotenv to manage environment variables and updated `.gitignore` to exclude `.env` files. Modified `Cargo.toml` to include `dotenv` and `env` dependencies, and updated the application to load configuration from environment variables at runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use dotenv::dotenv;
+use std::env;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, Rocket 🚀"
 }
 
+struct AppConfig {
+    app_key: String,
+    app_secret: String,
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    dotenv().expect("Failed to load .env file");
+    let app_key = env::var("APP_KEY").expect("APP_KEY not set");
+    let app_secret = env::var("APP_SECRET").expect("SECRET_KEY not set");
+
+    let config = AppConfig {
+        app_key,
+        app_secret,
+    };
+
+    rocket::build().mount("/", routes![index]).manage(config)
 }
