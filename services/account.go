f84713b2cfd1caec411cf3fe0cd3a3d59d73de49
IkHwan Im <ekankr2@naver.com>
2025-05-12 19:05:16 +0900

    계좌잔고 조회 성공

--- services/account.go
+++ services/account.go
@@ -32,6 +32,11 @@ func (s *KISService) GetAccountBalance(accountNo string) (map[string]interface{}
 	q.Add("OFL_YN", "N")                 // 오프라인여부(N: 미포함, Y: 포함)
 	q.Add("UNPR_DVSN", "01")             // 단가 구분 - 01: 평균단가, 02: 기준가
 	q.Add("FUND_STTL_ICLD_YN", "N")      // 펀드 결제분 포함 여부 - 일반적으로 "N"으로 설정
+	q.Add("CTX_AREA_FK100", "")          // 연속조회검색조건100
+	q.Add("CTX_AREA_NK100", "")          // 연속조회키100
+	q.Add("FNCG_AMT_AUTO_RDPT_YN", "N")  // 융자금액 자동상환여부
+	q.Add("PRCS_DVSN", "01")             // 처리구분 - 01: 조회, 02: 정정
+	q.Add("COST_ICLD_YN", "N")           // 비용포함여부
 	req.URL.RawQuery = q.Encode()
 
 	// 헤더 설정
@@ -40,6 +45,8 @@ func (s *KISService) GetAccountBalance(accountNo string) (map[string]interface{}
 	req.Header.Set("appkey", s.appKey)
 	req.Header.Set("appsecret", s.appSecret)
 	req.Header.Set("tr_id", "TTTC8434R") // 계좌 잔고 조회 TR ID
+	req.Header.Set("custtype", "P")      // 고객타입 - 개인: P, 법인: B
+	req.Header.Set("tr_cont", "")        // 연속거래 여부 - N: 초기, F: 최초, M: 중간, L: 마지막
 
 	// 요청 전송
 	resp, err := s.client.Do(req)
